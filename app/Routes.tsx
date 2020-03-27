81c95b20e180de492965b382e2e275f7d4abdc58
lakshankarunathilake <Manual@123>
2020-03-27 22:47:35 +0530

    Update routes

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -4,14 +4,15 @@ import routes from './constants/routes.json';
 import App from './containers/App';
 import LoginPage from './containers/LoginPage';
 import SignupPage from './containers/SignupPage';
+import HomePage from './containers/HomePage';
 
 export default function Routes() {
   return (
     <App>
       <Switch>
-        <Route path={routes.SIGNUP} component={SignupPage} />
+        <Route path={routes.SIGNUP} component={HomePage} />
         <Route path={routes.LOGIN} component={LoginPage} />
-        <Route path={routes.HOME} component={LoginPage} />
+        <Route path={routes.HOME} component={SignupPage} />
       </Switch>
     </App>
   );
