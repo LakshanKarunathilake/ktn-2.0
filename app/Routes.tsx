07049061fa43cbda346802e62cca03fda795840a
lakshankarunathilake <Manual@123>
2020-03-27 21:56:50 +0530

    Create login and Signup pages

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import LoginPage from './containers/LoginPage';
+import SignupPage from './containers/SignupPage';
 
 export default function Routes() {
   return (
     <App>
       <Switch>
-        <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.HOME} component={HomePage} />
+        <Route path={routes.SIGNUP} component={SignupPage} />
+        <Route path={routes.LOGIN} component={LoginPage} />
       </Switch>
     </App>
   );
---
+++ app/containers/SignupPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import SignUp from '../components/Signup/Signup';
+
+export default function SignupPage() {
+  return <SignUp />;
+}
