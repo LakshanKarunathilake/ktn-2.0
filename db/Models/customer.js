a0cd238ed25b0fdc905a2231f67c0beae16accf0
lakshankarunathilake <Manual@123>
2020-03-30 10:00:55 +0530

    Update customer model

--- db/Models/customer.js
+++ db/Models/customer.js
@@ -1,8 +1,13 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Customer = sequelize.define('Customer', {
-    name: DataTypes.STRING
-  }, {});
+  const Customer = sequelize.define(
+    'Customer',
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      address: { type: DataTypes.STRING, allowNull: false },
+      contactNumber: { type: DataTypes.STRING, allowNull: false }
+    },
+    {}
+  );
   Customer.associate = function(models) {
     // associations can be defined here
   };
