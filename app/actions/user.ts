5daf4d48bf69d82c4ad2336e0fd39f7d15309095
Lakshan-mac <manualsplash@gmail.com>
2020-04-04 22:38:59 +0530

    Update user actions

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -6,12 +6,12 @@ export const SIGNUP_USER = 'SIGNUP_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
 export const GETALL_USER = 'GETALL_USER';
 export const DELETE_USER = 'DELETE_USER';
-export const UPDATE_SIGNUP = 'UPDATE_SIGNUP';
 
-export function updateSignup(value: string) {
+export function updateSignup(key: string, value: string) {
   return (dispatch: Dispatch) => {
     dispatch({
-      type: UPDATE_SIGNUP,
+      type: `UPDATE_${key.toUpperCase()}`,
+      key,
       value
     });
   };
