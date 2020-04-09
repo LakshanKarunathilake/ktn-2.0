724712441fa452a351c5c60acafd6d85a666ee3d
Lakshan-mac <manualsplash@gmail.com>
2020-04-09 23:05:31 +0530

    Update method to create new user

--- app/services/user.ts
+++ app/services/user.ts
@@ -1,9 +1,23 @@
-import models from '../../db/Models/index';
+import { SignupView } from '../models/User';
+import DBService from './db';
 
 class UserService {
-  static createUser(username: string, password: string) {
-    console.log('');
+  static createUser(user: SignupView) {
+    console.log('mode', user);
+    const sequelize = DBService.getSequelize();
+    console.log('sequelize', sequelize.models);
+    sequelize
+      .model('User')
+      .create({ name: user.username, password: user.password, type: 1 })
+      .then((users: any) => {
+        console.log('user', users);
+      })
+      .catch((e: any) => {
+        console.log('error', e);
+      });
   }
+
+  static loginUser(username: string, password: string) {}
 }
 
 export default UserService;
