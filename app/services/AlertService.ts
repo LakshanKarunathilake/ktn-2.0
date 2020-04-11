8dc694932da116b6bb012715b56261a89d010ecc
Lakshan-mac <manualsplash@gmail.com>
2020-04-11 23:57:17 +0530

    Add success and error messages

--- app/services/AlertService.ts
+++ app/services/AlertService.ts
@@ -4,6 +4,10 @@ class AlertService {
   static successMessage(title: string, description: string) {
     swal(title, description, 'success');
   }
+
+  static errorMessage(title: string, description: string) {
+    swal(title, description, 'error');
+  }
 }
 
 export default AlertService;
