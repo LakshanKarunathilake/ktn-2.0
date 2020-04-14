e4e7f67ef663ce15a799e1181e16dfa851450ddc
Lakshan-mac <manualsplash@gmail.com>
2020-04-14 15:34:45 +0530

    Updata home page container

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
-import Dashboard from '../components/Home/Dashboard';
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { updateForm } from '../actions/user';
+import { inventoryStoreType } from '../reducers/types';
+import Home from '../components/Home/Home';
 
-export default function HomePage() {
-  return <Dashboard />;
+function mapStateToProps(state: inventoryStoreType) {
+  console.log('login state', state);
+  const { loginForm } = state;
+  return {
+    loginForm
+  };
 }
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return bindActionCreators(
+    {
+      updateForm
+    },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
