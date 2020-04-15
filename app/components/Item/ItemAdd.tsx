c3792ddabb22feb0a7f919a2c9501d1fd8be3891
Lakshan-mac <manualsplash@gmail.com>
2020-04-15 21:07:48 +0530

    Create item add template

--- app/components/Item/ItemAdd.tsx
+++ app/components/Item/ItemAdd.tsx
@@ -1,7 +1,89 @@
 import React from 'react';
+import {
+  Card,
+  TextField,
+  CardHeader,
+  CardContent,
+  Box,
+  Select,
+  MenuItem,
+  Typography,
+  CardActions,
+  Button
+} from '@material-ui/core';
 
 const ItemAdd = () => {
-  return <div>Item add works</div>;
+  return (
+    <>
+      <div style={{ height: '60%', display: 'flex' }}>
+        <Card
+          variant="outlined"
+          style={{
+            margin: '15px',
+            width: '70%'
+          }}
+        >
+          <CardHeader
+            title="New Item"
+            subheader="Add new item to inventory or edit exisiting item"
+          />
+          <CardContent>
+            <Box p={2} style={{ width: '80%' }}>
+              <TextField id="standard-basic" label="Part Number" fullWidth />
+            </Box>
+            <Box p={2} style={{ width: '40%' }}>
+              <TextField id="standard-basic" label="Category" fullWidth />
+            </Box>
+            <Box p={2}>
+              <TextField id="standard-basic" label="Vehicle" fullWidth />
+            </Box>
+
+            <Box p={2}>
+              <TextField
+                id="standard-basic"
+                fullWidth
+                variant="outlined"
+                disabled
+                value={'Description value will be auto generated'}
+              />
+            </Box>
+            <Box p={2} style={{ width: '50%' }}>
+              <Select label="Unit Type" value="Pcs" fullWidth>
+                <MenuItem value="Pcs">Pcs</MenuItem>
+                <MenuItem value="Set">Set</MenuItem>
+                <MenuItem value="Feet">Feet</MenuItem>
+                <MenuItem value="Meeter">Meeter</MenuItem>
+              </Select>
+            </Box>
+            <Box p={2} style={{ width: '50%' }}>
+              <TextField id="standard-basic" label="Location" fullWidth />
+            </Box>
+            <Box p={2}>
+              <Typography>
+                Please not that by default quantity will be 0, selling price
+                will be 0.00 and cost will be 0.00
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions style={{ float: 'right' }}>
+            <Button color="primary" variant="contained">
+              Save
+            </Button>
+            <Button color="secondary" variant="contained">
+              Cancel
+            </Button>
+          </CardActions>
+        </Card>
+        <Card variant="outlined" style={{ margin: '15px', width: '30%' }}>
+          <CardHeader
+            title="History of Additions"
+            subheader="Click to revert the item addition"
+          />
+        </Card>
+      </div>
+      <Card variant="outlined" style={{ margin: '15px', height: '30%' }}></Card>
+    </>
+  );
 };
 
 export default ItemAdd;
