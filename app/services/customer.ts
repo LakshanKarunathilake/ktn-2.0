c5c1bb580cbb8780fd7b0affb21a50a70b56fb58
Lakshan-mac <manualsplash@gmail.com>
2020-04-21 23:31:52 +0530

    Get customer from name

--- app/services/customer.ts
+++ app/services/customer.ts
@@ -6,10 +6,24 @@ const sequelize = DBService.getSequelize();
 export default class CustomerService {
   static getCustomers(name: string) {
     return sequelize.query(
-      `Select name from Users where name like '${name}%'`,
+      `Select name from Customers where name like '${name}%'`,
       {
         type: QueryTypes.SELECT
       }
     );
+  }
+
+  static async getCustomer(name: string) {
+    try {
+      const customer = await sequelize.model('Customer').findOne({
+        where: {
+          name
+        }
+      });
+      return customer;
+    } catch (e) {
+      console.log('Error in getting item', e);
+    }
+    return null;
   }
 }
