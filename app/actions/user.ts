d00d4c5f8811ccd3bc17408547ae078debd64176
Lakshan-mac <manualsplash@gmail.com>
2020-04-21 23:30:43 +0530

    Add user actions to form update

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -1,4 +1,6 @@
 import { Dispatch } from '../reducers/types';
+import { ItemAddView } from '../models/User';
+import { UPDATE_FROM_STATUS, UPDATE_FULL_FORM } from './item';
 
 export const EDIT_USER = 'EDIT_USER';
 export const LOGIN_USER = 'LOGIN_USER';
@@ -12,6 +14,24 @@ export function updateForm(key: string, value: string, form: string) {
     dispatch({
       type: `UPDATE_${form.toUpperCase()}_${key.toUpperCase()}`,
       key,
+      value
+    });
+  };
+}
+
+export function updateFullForm(values: ItemAddView) {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: UPDATE_FULL_FORM,
+      values
+    });
+  };
+}
+
+export function setFormDisabled(value: boolean) {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: UPDATE_FROM_STATUS,
       value
     });
   };
