349a3c32f63b20c8a95370550e49ce041339f2a7
Lakshan-mac <manualsplash@gmail.com>
2020-04-23 23:19:03 +0530

    Format seed value

--- db/migrations/20200329173950-create-invoice.js
+++ db/migrations/20200329173950-create-invoice.js
@@ -9,10 +9,13 @@ module.exports = {
       },
       invoice: { type: Sequelize.STRING, allowNull: false, unique: true },
       total: { type: Sequelize.DECIMAL(10, 2), allowNull: false },
-      discount: { type: Sequelize.DECIMAL(10, 2), allowNull: false },
+      discount: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0
+      },
       grand: { type: Sequelize.DECIMAL(10, 2), allowNull: false },
       credit: { type: Sequelize.BOOLEAN, allowNull: false },
-      returned: { type: Sequelize.DECIMAL(10, 2), allowNull: false },
       note: { type: Sequelize.STRING },
       createdAt: {
         allowNull: false,
