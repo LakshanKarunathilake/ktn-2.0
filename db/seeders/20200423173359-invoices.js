30e844ec56f93f0355962e73b0d3c16cd0fac411
Lakshan-mac <manualsplash@gmail.com>
2020-04-25 23:26:36 +0530

    update invoice seed

--- db/seeders/20200423173359-invoices.js
+++ db/seeders/20200423173359-invoices.js
@@ -1,14 +1,25 @@
 const invoices = require('./data/Invoices');
+let customers = require('./data/Customers');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    customers = customers.map(val => val.customer_code.toString());
+
     const newRecords = invoices.map(val => {
+      let customerId;
+      if (customers.includes(val.customer_code)) {
+        customerId = parseInt(val.customer_code, 10);
+      } else {
+        customerId = 1;
+      }
+      console.log('c',customerId);
       return {
         invoice: val.invoice_id,
         total: val.total,
         grand: val.grandTotal,
         credit: val.Status,
         note: val.additional_note,
+        customerId,
         createdAt: new Date(val.orderDate),
         updatedAt: new Date(val.orderDate)
       };
