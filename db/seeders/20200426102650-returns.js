24a6afe358e6d3d13da70a2846339a494db12abe
Lakshan-mac <manualsplash@gmail.com>
2020-04-26 16:49:06 +0530

    Update return seeder

--- db/seeders/20200426102650-returns.js
+++ db/seeders/20200426102650-returns.js
@@ -1,17 +1,21 @@
-'use strict';
+const returns = require('./data/Returns');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    const newRecords = returns
+      .filter(val => val.qty > 0 && val.item_code !== '')
+      .map(val => {
+        return {
+          itemCode: val.item_code,
+          qty: val.qty,
+          unit: val.qty > 0 ? val.amount / val.qty : val.amount,
+          reason: val.Reason,
+          createdAt: new Date(val.date),
+          updatedAt: new Date(val.date),
+          invoiceId: val.invoice_id
+        };
+      });
+    return queryInterface.bulkInsert('Returns', newRecords, {});
   },
 
   down: (queryInterface, Sequelize) => {
