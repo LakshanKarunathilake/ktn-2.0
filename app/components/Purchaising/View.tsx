0804896dea1a4c86d2477619de9fdfebfc696e8d
Lakshan-mac <manualsplash@gmail.com>
2020-04-26 23:40:53 +0530

    Add stepper layout

--- app/components/Purchaising/View.tsx
+++ app/components/Purchaising/View.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Tabs, Card } from 'antd';
+import { Tabs, Card, Steps, Typography } from 'antd';
+import {
+  UserOutlined,
+  SolutionOutlined,
+  SmileOutlined
+} from '@ant-design/icons';
 
 const { TabPane } = Tabs;
+const { Step } = Steps;
+const { Text, Title } = Typography;
 
 function callback(key) {
   console.log(key);
@@ -12,7 +19,27 @@ const View = () => {
     <Card style={{ height: '100%' }}>
       <Tabs defaultActiveKey="1" onChange={callback}>
         <TabPane tab="New Purchase" key="1">
-          Content of Tab Pane 1
+          <Card>
+            <Title level={4}>You are about to add a purchase</Title>
+            <Text>You have to Follow Three Steps, to complete a purchase</Text>
+          </Card>
+          <Steps>
+            <Step
+              status="finish"
+              title="Purchase overview"
+              icon={<UserOutlined />}
+            />
+            <Step
+              status="finish"
+              title="Detailed Purchase"
+              icon={<SolutionOutlined />}
+            />
+            <Step
+              status="finish"
+              title="Confirmation"
+              icon={<SmileOutlined />}
+            />
+          </Steps>
         </TabPane>
         <TabPane tab="Previous Purchase" key="2">
           Content of Tab Pane 2
