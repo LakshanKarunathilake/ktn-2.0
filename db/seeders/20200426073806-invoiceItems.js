54b4def6997daf9f98853aaa7895e927eb5978b0
Lakshan-mac <manualsplash@gmail.com>
2020-04-26 15:54:01 +0530

    Update invoice item seed

--- db/seeders/20200426073806-invoiceItems.js
+++ db/seeders/20200426073806-invoiceItems.js
@@ -1,17 +1,30 @@
-'use strict';
+const invoiceItems = require('./data/InvoiceItems');
 
+/**
+ * Delete non existing record from invoices
+ *
+ * item code : 3PK 0760
+ * invoice : A2790
+ * @type {{up: (function(*, *): <Object> | *), down: down}}
+ */
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    const newRecords = invoiceItems
+      .filter(
+        val => val.qty > 0 && val.returnable_qty >= 0 && val.item_code !== ''
+      )
+      .map(val => {
+        return {
+          code: val.Item_code,
+          qty: val.qty,
+          returnedQty: val.qty - val.returnable_qty,
+          selling: val.total / val.qty,
+          invoiceId: val.invoice_id,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        };
+      });
+    return queryInterface.bulkInsert('InvoiceItems', newRecords, {});
   },
 
   down: (queryInterface, Sequelize) => {
