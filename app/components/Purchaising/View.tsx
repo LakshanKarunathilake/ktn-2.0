4a5054415d518c9ece17ddd3ce8ff251640cc9f7
Lakshan-mac <manualsplash@gmail.com>
2020-04-29 22:31:18 +0530

    Pass props down to addPurchase

--- app/components/Purchaising/View.tsx
+++ app/components/Purchaising/View.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Card } from 'antd';
 import NewPurchase from './NewPurchase';
+import Purchase from '../../models/Purchase';
 
 const { TabPane } = Tabs;
 
@@ -8,12 +9,17 @@ function callback(key) {
   console.log(key);
 }
 
-const View = () => {
+const View = (props: {
+  purchase: Purchase;
+  updateForm: (key: string, value: string) => void;
+}) => {
+  console.log('view props', props);
+  const { purchase, updateForm } = props;
   return (
     <Card style={{ height: '100%' }}>
       <Tabs defaultActiveKey="1" onChange={callback}>
         <TabPane tab="New Purchase" key="1">
-          <NewPurchase />
+          <NewPurchase purchase={purchase} updateForm={updateForm} />
         </TabPane>
         <TabPane tab="Previous Purchase" key="2">
           Content of Tab Pane 2
