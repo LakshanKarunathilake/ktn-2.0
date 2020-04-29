3e46af3988eb894a63f00af2fcb1fb0965d9afd3
Lakshan-mac <manualsplash@gmail.com>
2020-04-29 23:15:33 +0530

    Move overall information to separate component

--- app/components/Purchaising/NewPurchase.tsx
+++ app/components/Purchaising/NewPurchase.tsx
@@ -16,10 +16,9 @@ import {
 import uid from 'uid';
 import PurchaseService from '../../services/purchase';
 import Purchase from '../../models/Purchase';
+import OverallInfo from '../Item/InformationSteps/overallInfo';
 
 const { Step } = Steps;
-const { Item } = Form;
-const { Title, Text } = Typography;
 
 const NewPurchase = (props: {
   purchase: Purchase;
@@ -28,22 +27,7 @@ const NewPurchase = (props: {
   console.log('new purch', props);
   const { purchase, updateForm } = props;
   const [currentStep, setCurrentStep] = useState(0);
-  const [names, setNames] = useState<{ value: string }[]>([]);
-  const [allNames, setAllNames] = useState<{ value: string }[]>([]);
 
-  useEffect(() => {
-    PurchaseService.getSuppliers()
-      .then((val: any) => {
-        const updated = val.map((v: any) => {
-          return { value: v.name };
-        });
-        setNames(updated);
-        return setAllNames(updated);
-      })
-      .catch((e: any) => {
-        console.log('error', e);
-      });
-  }, []);
   const next = () => {
     const current = currentStep + 1;
     setCurrentStep(current);
@@ -54,81 +38,10 @@ const NewPurchase = (props: {
     setCurrentStep(current);
   };
 
-  const onItemSearch = (searchText: string) => {
-    if (searchText !== '') {
-      setNames(
-        allNames.filter(value =>
-          value.value.toUpperCase().includes(searchText.toUpperCase())
-        )
-      );
-    }
-  };
-
   const steps = [
     {
       title: 'Purchase overview',
-      content: (
-        <>
-          <Card style={{ marginBottom: 50 }}>
-            <Title level={4}>New purchase add note</Title>
-            <ul>
-              <li>
-                <Text>Make sure you select the correct Supplier</Text>
-              </li>
-              <li>
-                <Text>
-                  If supplier is not there and new one but you need Super user
-                  password
-                </Text>
-              </li>
-              <li>
-                <Text>
-                  Invoice total - discount should match the total of purchase
-                  item costs
-                </Text>
-              </li>
-            </ul>
-          </Card>
-          <Form>
-            <Item>
-              <AutoComplete
-                options={names}
-                style={{ width: '40vw' }}
-                onSearch={onItemSearch}
-                placeholder="Retailer Name"
-                value={purchase.invoiceNo}
-                onChange={(value: string) => {
-                  updateForm('invoiceNo', value);
-                }}
-              />
-            </Item>
-            <Item>
-              <Input
-                style={{ width: '40vw' }}
-                addonBefore="Rs."
-                placeholder="Invoice Total"
-                value={purchase.total}
-                type="number"
-                onChange={(event: any) => {
-                  updateForm('total', event.target.value);
-                }}
-              />
-            </Item>
-            <Item>
-              <Input
-                style={{ width: '40vw' }}
-                addonAfter="%"
-                placeholder="Discount Percentage"
-                value={purchase.discount}
-                type="number"
-                onChange={(event: any) => {
-                  updateForm('discount', event.target.value);
-                }}
-              />
-            </Item>
-          </Form>
-        </>
-      ),
+      content: <OverallInfo purchase={purchase} updateForm={updateForm} />,
       icon: <UserOutlined />
     },
     {
