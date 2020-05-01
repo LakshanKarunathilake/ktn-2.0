bac9bcabfc575777dcc124e15fcb0ed62da13680
Lakshan-mac <manualsplash@gmail.com>
2020-05-01 23:39:10 +0530

    Implement methods to add to database

--- app/services/purchase.ts
+++ app/services/purchase.ts
@@ -1,10 +1,21 @@
 import DBService from './db';
+import Purchase from '../models/Purchase';
 
 const db = DBService.getSequelize();
 
 class PurchaseService {
   static getSuppliers() {
     return db.model('Company').findAll({ attributes: ['name'] });
+  }
+
+  static addPurchase(purchase: Purchase) {
+    return db.model('Purchase').create({
+      purchase
+    });
+  }
+
+  static addPurchaseItems(purchase: Purchase) {
+    return db.model('Purchase').bulkCreate([purchase.items]);
   }
 }
 
