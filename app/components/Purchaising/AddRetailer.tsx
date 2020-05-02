e275a6eb461f9ae8a64871b5c0f5a5da7b13820f
Lakshan-mac <manualsplash@gmail.com>
2020-05-02 23:09:27 +0530

    Form update for adding retailer

--- app/components/Purchaising/AddRetailer.tsx
+++ app/components/Purchaising/AddRetailer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Typography, Form, AutoComplete } from 'antd';
+import { Card, Typography, Form, AutoComplete, Input, Button } from 'antd';
 import PurchaseService from '../../services/purchase';
+import swal from 'sweetalert';
 
 const { Text, Title } = Typography;
 const { Item } = Form;
@@ -8,6 +9,12 @@ const { Item } = Form;
 const AddRetailer = () => {
   const [names, setNames] = useState<{ value: string }[]>([]);
   const [allNames, setAllNames] = useState<{ value: string }[]>([]);
+  const [address, setAddress] = useState();
+  const [name, setName] = useState();
+  const [contactNumber, setContactNumber] = useState();
+  const [editing, setEditing] = useState(true);
+  const [formaDisable, setFormDisable] = useState(true);
+  const [supplierInstance, setSupplierInstance] = useState(true);
 
   useEffect(() => {
     PurchaseService.getSuppliers()
@@ -25,17 +32,50 @@ const AddRetailer = () => {
 
   const onItemSearch = (searchText: string) => {
     if (searchText !== '') {
-      setNames(
-        allNames.filter(value =>
-          value.value.toUpperCase().includes(searchText.toUpperCase())
-        )
+      const similarRecords = allNames.filter(value =>
+        value.value.toUpperCase().includes(searchText.toUpperCase())
       );
+      if (similarRecords.length > 0) {
+        setNames(similarRecords);
+      } else {
+        setEditing(false);
+        setFormDisable(false);
+      }
+    }
+  };
+
+  const displayConfirmMessage = () => {
+    return swal({
+      title: 'Confirm Edit',
+      text:
+        'Once deleted, you will not be able to recover this imaginary file!',
+      icon: 'warning',
+      buttons: ['Cancel', 'Proceed']
+    });
+  };
+
+  const clearForm = () => {
+    setAddress('');
+    setContactNumber('');
+  };
+
+  const onSupplierSelect = async (value: string) => {
+    if ((await displayConfirmMessage()) === true) {
+      setFormDisable(false);
+      setEditing(true);
+      const supplier = await PurchaseService.getSupplier(value);
+      setSupplierInstance(supplier);
+      setAddress(supplier.address);
+      setContactNumber(supplier.contactNumber);
+    } else {
+      clearForm();
+      setEditing(false);
     }
   };
 
   return (
     <>
-      <Card>
+      <Card style={{ marginBottom: 20 }}>
         <Title level={4}>Notes on adding new retailer</Title>
         <ul>
           <li>
@@ -55,10 +95,52 @@ const AddRetailer = () => {
             options={names}
             style={{ width: '40vw' }}
             onSearch={onItemSearch}
+            onSelect={onSupplierSelect}
+            value={name}
+            onChange={val => setName(val)}
             placeholder="Retailer Name"
           />
         </Item>
+        <Item>
+          <Input
+            style={{ marginRight: 20, width: '60vw' }}
+            placeholder="Address"
+            value={address}
+            disabled={formaDisable}
+            onChange={(event: any) => {
+              setAddress(event.target.value);
+            }}
+          />
+        </Item>
+        <Item>
+          <Input
+            style={{ marginRight: 20, width: '60vw' }}
+            placeholder="Contact Numbers"
+            value={contactNumber}
+            disabled={formaDisable}
+            onChange={(event: any) => {
+              setContactNumber(event.target.value);
+            }}
+          />
+        </Item>
       </Form>
+      <section>
+        {!editing && (
+          <Button
+            style={{ margin: 5 }}
+            type="primary"
+            onClick={() => {
+              PurchaseService.addSupplier({ name, address, contactNumber });
+            }}
+          >
+            Add
+          </Button>
+        )}
+        {editing && <Button style={{ margin: 5 }}>Update</Button>}
+        <Button style={{ margin: 5 }} type="danger" onClick={clearForm}>
+          Cancel
+        </Button>
+      </section>
     </>
   );
 };
