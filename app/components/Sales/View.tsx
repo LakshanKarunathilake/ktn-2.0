4719ca2e91393eae78e631dc60f2fc60754f65df
Lakshan-mac <manualsplash@gmail.com>
2020-05-11 23:11:17 +0530

    Initial view for sales

---
+++ app/components/Sales/View.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Text from 'antd/es/typography/Text';
+
+const View = () => {
+  return (
+    <div>
+      <Text>Sales</Text>
+    </div>
+  );
+};
+
+export default View;
