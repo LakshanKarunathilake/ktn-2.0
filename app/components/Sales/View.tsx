90c4dd497ccfee5d84e3f5b1f52df6bc1539880f
Lakshan-mac <manualsplash@gmail.com>
2020-05-12 20:56:13 +0530

    Transfered to functional component

--- app/components/Sales/View.tsx
+++ app/components/Sales/View.tsx
@@ -1,46 +1,40 @@
 import { Tabs, Button } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const { TabPane } = Tabs;
 
-class View extends React.Component {
-  constructor(props) {
-    super(props);
-    this.newTabIndex = 0;
-    const panes = [
+const View = () => {
+  const [panes, setPanes] = useState([]);
+  const [newTabIndex, setNewTabIndex] = useState(0);
+  const [activeKey, setActiveKey] = useState();
+
+  useEffect(() => {
+    setPanes([
       { title: 'Tab 1', content: 'Content of Tab Pane 1', key: '1' },
       { title: 'Tab 2', content: 'Content of Tab Pane 2', key: '2' }
-    ];
-    this.state = {
-      activeKey: panes[0].key,
-      panes
-    };
-  }
+    ]);
+    setActiveKey(panes[0]);
+  }, []);
 
-  onChange = activeKey => {
-    this.setState({ activeKey });
+  const add = () => {
+    const newActiveKey = `newTab${setNewTabIndex(newTabIndex + 1)}`;
+    setPanes(prevState => {
+      return [
+        ...prevState,
+        { title: 'New Tab', content: 'New Tab Pane', key: activeKey }
+      ];
+    });
+    setActiveKey(newActiveKey);
   };
 
-  onEdit = (targetKey, action) => {
-    this[action](targetKey);
-  };
-
-  add = () => {
-    const { panes } = this.state;
-    const activeKey = `newTab${this.newTabIndex++}`;
-    panes.push({ title: 'New Tab', content: 'New Tab Pane', key: activeKey });
-    this.setState({ panes, activeKey });
-  };
-
-  remove = targetKey => {
-    let { activeKey } = this.state;
+  const remove = targetKey => {
     let lastIndex;
-    this.state.panes.forEach((pane, i) => {
+    panes.forEach((pane, i) => {
       if (pane.key === targetKey) {
         lastIndex = i - 1;
       }
     });
-    const panes = this.state.panes.filter(pane => pane.key !== targetKey);
+    const panes = panes.filter(pane => pane.key !== targetKey);
     if (panes.length && activeKey === targetKey) {
       if (lastIndex >= 0) {
         activeKey = panes[lastIndex].key;
@@ -51,28 +45,34 @@ class View extends React.Component {
     this.setState({ panes, activeKey });
   };
 
-  render() {
-    return (
-      <div>
-        <div style={{ marginBottom: 16 }}>
-          <Button onClick={this.add}>ADD</Button>
-        </div>
-        <Tabs
-          hideAdd
-          onChange={this.onChange}
-          activeKey={this.state.activeKey}
-          type="editable-card"
-          onEdit={this.onEdit}
-        >
-          {this.state.panes.map(pane => (
-            <TabPane tab={pane.title} key={pane.key}>
-              {pane.content}
-            </TabPane>
-          ))}
-        </Tabs>
+  const onEdit = (targetKey, action) => {
+    if (action === 'addd') {
+      add(targetKey);
+    } else {
+      remove(targetKey);
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ marginBottom: 16 }}>
+        <Button onClick={add}>ADD</Button>
       </div>
-    );
-  }
-}
+      <Tabs
+        hideAdd
+        onChange={key => setActiveKey(key)}
+        activeKey={activeKey}
+        type="editable-card"
+        onEdit={onEdit}
+      >
+        {panes.map(pane => (
+          <TabPane tab={pane.title} key={pane.key}>
+            {pane.content}
+          </TabPane>
+        ))}
+      </Tabs>
+    </div>
+  );
+};
 
 export default View;
