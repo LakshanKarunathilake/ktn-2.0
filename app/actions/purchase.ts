4d602a96f17f2b0b38461087cec8df1e7c26e4e2
Lakshan-mac <manualsplash@gmail.com>
2020-06-02 12:43:06 +0530

    Add anytype to form field

--- app/actions/purchase.ts
+++ app/actions/purchase.ts
@@ -3,13 +3,12 @@ import { Dispatch } from '../reducers/types';
 export const UPDATE_FORM = 'UPDATE_FORM';
 export const DISABLE_FORM = 'DISABLE_FORM';
 
-export function updateForm(key: string, value: string, form: string) {
+export function updateForm(key: string, value: any) {
   return (dispatch: Dispatch) => {
     dispatch({
       type: UPDATE_FORM,
       key,
-      value,
-      form
+      value
     });
   };
 }
